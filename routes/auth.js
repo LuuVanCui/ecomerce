5ca2e51f798a3b1319b245de933038dbc3a69791
Luu Van Cui <luuvancui10a1516@gmail.com>
2021-09-05 11:16:05 +0700

    protect middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { register, login } = require("../controllers/auth");
+const { register, login, logout } = require("../controllers/auth");
+const { protect } = require('../middlewares/auth');
 const router = express.Router();
 
 router.post("/register", register);
 router.post('/login', login);
+router.get('/logout', logout);
+router.get('/getme', protect);
 
 module.exports = router;
